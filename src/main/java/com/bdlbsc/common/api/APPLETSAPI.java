04505caf367af0354f532f9b1061641617463fb4
m_maohuawei <maohuawei@bdlbsc.com>
2020-04-14 14:21:57 +0800

    更新字段

--- src/main/java/com/bdlbsc/common/api/APPLETSAPI.java
+++ src/main/java/com/bdlbsc/common/api/APPLETSAPI.java
@@ -3,6 +3,6 @@ package com.bdlbsc.common.api;
 public interface APPLETSAPI {
 
     // 获取小程序列表
-    String APPLETS_GET_PLUGINS = "applets/get/plguins";
+    String APPLETS_GET_APPS = "applets/get/apps";
 
 }
--- src/main/java/com/bdlbsc/common/user/UserLoginPhonenumberRequest.java
+++ src/main/java/com/bdlbsc/common/user/UserLoginPhonenumberRequest.java
@@ -1,12 +1,13 @@
 package com.bdlbsc.common.user;
 
 import com.bdlbsc.common.base.BaseRequest;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
+@EqualsAndHashCode(callSuper = false)
 public class UserLoginPhonenumberRequest extends BaseRequest {
     private String phone_number;
     private String verification_code;
